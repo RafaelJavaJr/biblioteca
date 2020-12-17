6f8095d2c6f3ae05683bea9fa59d800ef080107f
Rafael <rafabb1350@gmail.com>
2020-12-17 13:38:23 -0300

    Conecção com o banco e index

---
+++ src/br/com/rafael/connection/DB.java
@@ -0,0 +1,23 @@
+package br.com.rafael.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DB {
+	private static String banco = "jdbc:postgresql://localhost:5432/biblioteca?autoReconnect=true";
+	private static String user = "postgres";
+	private static String password = "admin";	
+
+	public static Connection getCon() {
+		Connection con = null;		
+		try {
+			Class.forName("org.postgresql.Driver");
+			con = DriverManager.getConnection(banco, user, password);
+			System.out.println("Conectou");
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return con;
+	}
+}
