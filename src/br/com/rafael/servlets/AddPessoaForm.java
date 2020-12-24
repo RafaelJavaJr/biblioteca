a7ba3ac17f897ab4dc449530435baf57d6a96c71
Rafael <rafabb1350@gmail.com>
2020-12-24 15:16:24 -0300

    AddPessoaForm

--- src/br/com/rafael/servlets/AddPessoaForm.java
+++ src/br/com/rafael/servlets/AddPessoaForm.java
@@ -1,41 +1,36 @@
 package br.com.rafael.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-/**
- * Servlet implementation class AddPessoaForm
- */
-@WebServlet("/AddPessoaForm")
+@WebServlet("/AddLibrarianForm")
 public class AddPessoaForm extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AddPessoaForm() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		out.print("<!DOCTYPE html>");
+		out.print("<html>");
+		out.println("<head>");
+		out.println("<title>Add Librarian Form</title>");
+		out.println("<link rel='stylesheet' href='bootstrap.min.css'/>");
+		out.println("</head>");
+		out.println("<body>");
+		
+		request.getRequestDispatcher("navadmin.html").include(request, response);
+		out.println("<div class='container'>");
+		request.getRequestDispatcher("addlibrarianform.html").include(request, response);
+		out.println("</div>");
+		
+		
+		
+		request.getRequestDispatcher("footer.html").include(request, response);
+		out.close();
 	}
 
 }
