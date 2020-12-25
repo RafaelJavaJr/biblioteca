53e3216a68c1acbefac773517abe7c5914a03432
Rafael <rafabb1350@gmail.com>
2020-12-25 12:13:25 -0300

    Delete Pessoa

--- src/br/com/rafael/servlets/DeletePessoa.java
+++ src/br/com/rafael/servlets/DeletePessoa.java
@@ -1,5 +1,4 @@
 package br.com.rafael.servlets;
-
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,35 +6,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class DeletePessoa
- */
-@WebServlet("/DeletePessoa")
+import br.com.rafael.dao.PessoaDao;
+@WebServlet("/DeleteLibrarian")
 public class DeletePessoa extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DeletePessoa() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String sid=request.getParameter("id");
+		int id=Integer.parseInt(sid);
+		PessoaDao.delete(id);
+		response.sendRedirect("ViewLibrarian");
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
