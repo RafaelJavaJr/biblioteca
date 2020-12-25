a93e06abeacb23c1e83d02294425dacc25828da3
Rafael <rafabb1350@gmail.com>
2020-12-25 11:56:12 -0300

    Delete Livro

--- src/br/com/rafael/servlets/DeleteLivro.java
+++ src/br/com/rafael/servlets/DeleteLivro.java
@@ -7,35 +7,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class DeleteLivro
- */
-@WebServlet("/DeleteLivro")
+import br.com.rafael.dao.LivroDao;
+@WebServlet("/DeleteBook")
 public class DeleteLivro extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DeleteLivro() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		LivroDao.delete(request.getParameter("registro"));
+		response.sendRedirect("ViewBook");
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
