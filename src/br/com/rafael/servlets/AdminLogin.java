da8fc628726c07f6d9a331548decd8baa75ee911
Rafael <rafabb1350@gmail.com>
2020-12-25 11:38:26 -0300

    Admin Login

--- src/br/com/rafael/servlets/AdminLogin.java
+++ src/br/com/rafael/servlets/AdminLogin.java
@@ -9,10 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-/**
- * Servlet implementation class AdminLogin
- */
 @WebServlet("/AdminLogin")
 public class AdminLogin extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
