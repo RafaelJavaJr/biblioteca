33c41d94daf7418abc52a066a87d8f8635410039
Rafael <Rafael@Casa>
2020-12-26 03:10:31 -0300

    Devolver Livro

--- src/br/com/rafael/servlets/DevolverLivro.java
+++ src/br/com/rafael/servlets/DevolverLivro.java
@@ -1,41 +1,46 @@
 package br.com.rafael.servlets;
-
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class DevolverLivro
- */
-@WebServlet("/DevolverLivro")
+import br.com.rafael.beans.EmprestimoLivroBean;
+import br.com.rafael.dao.LivroDao;
+@WebServlet("/ReturnBook")
 public class DevolverLivro extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DevolverLivro() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		out.print("<!DOCTYPE html>");
+		out.print("<html>");
+		out.println("<head>");
+		out.println("<title>Return Book</title>");
+		out.println("<link rel='stylesheet' href='bootstrap.min.css'/>");
+		out.println("</head>");
+		out.println("<body>");
+		request.getRequestDispatcher("navlibrarian.html").include(request, response);
+		
+		out.println("<div class='container'>");
+		String callno=request.getParameter("registro");
+		String sestudanteid=request.getParameter("estudanteid");
+		int studentid=Integer.parseInt(sestudanteid);
+		
+		int i=LivroDao.returnBook(callno,studentid);
+		if(i>0){
+			out.println("<h3>Book returned successfully</h3>");
+		}else{
+			out.println("<h3>Sorry, unable to return book.</h3><p>We may have sortage of books. Kindly visit later.</p>");
+		}
+		out.println("</div>");
+		
+		
+		request.getRequestDispatcher("footer.html").include(request, response);
+		out.close();
 	}
 
 }
