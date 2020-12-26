5548e12b3a8fd665be303ced1bbc0dc6dc0d5ab8
Rafael <Rafael@Casa>
2020-12-26 07:53:52 -0300

    Edit pessoa

--- src/br/com/rafael/servlets/EditPessoa.java
+++ src/br/com/rafael/servlets/EditPessoa.java
@@ -1,41 +1,29 @@
 package br.com.rafael.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class EditPessoa
- */
-@WebServlet("/EditPessoa")
+import br.com.rafael.beans.PessoaBean;
+import br.com.rafael.dao.PessoaDao;
+@WebServlet("/EditLibrarian")
 public class EditPessoa extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public EditPessoa() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String sid=request.getParameter("id");
+		int id=Integer.parseInt(sid);
+		String name=request.getParameter("nome");
+		String email=request.getParameter("email");
+		String password=request.getParameter("senha");
+		String smobile=request.getParameter("telefone");
+		long telefone=Long.parseLong(smobile);
+		PessoaBean bean=new PessoaBean(id,name, email, password, telefone);
+		PessoaDao.update(bean);
+		response.sendRedirect("ViewLibrarian");
 	}
 
 }
