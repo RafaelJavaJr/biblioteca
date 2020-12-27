bcbaaf6a5304cbe04e4056f27577e1b291eff878
Rafael <Rafael@Casa>
2020-12-27 11:34:18 -0300

    Emprestimo Livro

--- src/br/com/rafael/servlets/EmprestimoLivro.java
+++ src/br/com/rafael/servlets/EmprestimoLivro.java
@@ -1,41 +1,51 @@
 package br.com.rafael.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class EmprestimoLivro
- */
-@WebServlet("/EmprestimoLivro")
+import br.com.rafael.beans.LivroBean;
+import br.com.rafael.beans.EmprestimoLivroBean;
+import br.com.rafael.dao.LivroDao;
+@WebServlet("/IssueBook")
 public class EmprestimoLivro extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public EmprestimoLivro() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		out.print("<!DOCTYPE html>");
+		out.print("<html>");
+		out.println("<head>");
+		out.println("<title>Add Book Form</title>");
+		out.println("<link rel='stylesheet' href='bootstrap.min.css'/>");
+		out.println("</head>");
+		out.println("<body>");
+		request.getRequestDispatcher("navlibrarian.html").include(request, response);
+		
+		out.println("<div class='container'>");
+		String registro=request.getParameter("registro");
+		String estudanteid=request.getParameter("estudanteid");
+		String estudantenome=request.getParameter("estudantenome");
+		String sestudantetelefone=request.getParameter("estudantetelefone");
+		long estudantetelefone=Long.parseLong(sestudantetelefone);
+		
+		EmprestimoLivroBean bean=new EmprestimoLivroBean(registro,estudanteid,estudantenome,estudantetelefone);
+		int i=LivroDao.issueBook(bean);
+		if(i>0){
+			out.println("<h3>Book issued successfully</h3>");
+		}else{
+			out.println("<h3>Sorry, unable to issue book.</h3><p>We may have sortage of books. Kindly visit later.</p>");
+		}
+		out.println("</div>");
+		
+		
+		request.getRequestDispatcher("footer.html").include(request, response);
+		out.close();
 	}
 
 }
