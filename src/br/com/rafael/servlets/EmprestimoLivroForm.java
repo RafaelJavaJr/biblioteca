d32cc05d35aacc708b9e8a9618a0f53e3c67679d
Rafael <Rafael@Casa>
2020-12-27 12:24:52 -0300

    Emprestimo Livro Form

--- src/br/com/rafael/servlets/EmprestimoLivroForm.java
+++ src/br/com/rafael/servlets/EmprestimoLivroForm.java
@@ -1,41 +1,34 @@
 package br.com.rafael.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-/**
- * Servlet implementation class EmprestimoLivroForm
- */
-@WebServlet("/EmprestimoLivroForm")
+@WebServlet("/IssueBookForm")
 public class EmprestimoLivroForm extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public EmprestimoLivroForm() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		out.print("<!DOCTYPE html>");
+		out.print("<html>");
+		out.println("<head>");
+		out.println("<title>Issue Book Form</title>");
+		out.println("<link rel='stylesheet' href='bootstrap.min.css'/>");
+		out.println("</head>");
+		out.println("<body>");
+		request.getRequestDispatcher("navlibrarian.html").include(request, response);
+		
+		out.println("<div class='container'>");
+		request.getRequestDispatcher("issuebookform.html").include(request, response);
+		out.println("</div>");
+		
+		
+		request.getRequestDispatcher("footer.html").include(request, response);
+		out.close();
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
