705a2b4ca304008b24a2fc8c1380171862b05aae
Rafael <rafabb1350@gmail.com>
2020-12-30 14:45:44 -0300

    Lista livro

--- src/br/com/rafael/servlets/ListaLivro.java
+++ src/br/com/rafael/servlets/ListaLivro.java
@@ -1,41 +1,50 @@
 package br.com.rafael.servlets;
 
+
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ListaLivro
- */
-@WebServlet("/ListaLivro")
+import br.com.rafael.beans.LivroBean;
+import br.com.rafael.beans.PessoaBean;
+import br.com.rafael.dao.LivroDao;
+import br.com.rafael.dao.PessoaDao;
+@WebServlet("/ViewBook")
 public class ListaLivro extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ListaLivro() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		out.print("<!DOCTYPE html>");
+		out.print("<html>");
+		out.println("<head>");
+		out.println("<title>View Book</title>");
+		out.println("<link rel='stylesheet' href='bootstrap.min.css'/>");
+		out.println("</head>");
+		out.println("<body>");
+		request.getRequestDispatcher("navlibrarian.html").include(request, response);
+		
+		out.println("<div class='container'>");
+		
+		List<LivroBean> list=LivroDao.view();
+		
+		out.println("<table class='table table-bordered table-striped'>");
+		out.println("<tr><th>Callno</th><th>Name</th><th>Author</th><th>Publisher</th><th>Quantity</th><th>Issued</th><th>Delete</th></tr>");
+		for(LivroBean bean:list){
+			out.println("<tr><td>"+bean.getRegistro()+"</td><td>"+bean.getNome()+"</td><td>"+bean.getAutor()+"</td><td>"+bean.getEditora()+"</td><td>"+bean.getQuantidade()+"</td><td>"+bean.getEmprestimo()+"</td><td><a href='DeleteLivro?registro="+bean.getRegistro()+"'>Delete</a></td></tr>");
+		}
+		out.println("</table>");
+		
+		out.println("</div>");
+		
+		
+		request.getRequestDispatcher("footer.html").include(request, response);
+		out.close();
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
