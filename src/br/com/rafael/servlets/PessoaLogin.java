26b26ffd0d9b7c295ac6870fb3c65005280bedc1
Rafael <rafabb1350@gmail.com>
2020-12-31 11:00:50 -0300

    Pessoa Login

--- src/br/com/rafael/servlets/PessoaLogin.java
+++ src/br/com/rafael/servlets/PessoaLogin.java
@@ -1,41 +1,51 @@
 package br.com.rafael.servlets;
 
+
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class PessoaLogin
- */
-@WebServlet("/PessoaLogin")
+import br.com.rafael.dao.PessoaDao;;
+@WebServlet("/LibrarianLogin")
 public class PessoaLogin extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public PessoaLogin() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		out.print("<!DOCTYPE html>");
+		out.print("<html>");
+		out.println("<head>");
+		out.println("<title>Librarian Section</title>");
+		out.println("<link rel='stylesheet' href='bootstrap.min.css'/>");
+		out.println("</head>");
+		out.println("<body>");
+		
+		String email=request.getParameter("email");
+		String password=request.getParameter("password");
+		if(PessoaDao.authenticate(email, password)){
+			HttpSession session=request.getSession();
+			session.setAttribute("email",email);
+			
+			request.getRequestDispatcher("navlibrarian.html").include(request, response);
+			request.getRequestDispatcher("librariancarousel.html").include(request, response);
+			
+		}else{
+			request.getRequestDispatcher("navhome.html").include(request, response);
+			out.println("<div class='container'>");
+			out.println("<h3>Username or password error</h3>");
+			request.getRequestDispatcher("librarianloginform.html").include(request, response);
+			out.println("</div>");
+		}
+		
+		
+		request.getRequestDispatcher("footer.html").include(request, response);
+		out.close();
 	}
 
 }
